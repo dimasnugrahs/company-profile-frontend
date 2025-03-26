76a43f57c1c16f06a12c590313364f5406b7510b
dimasnugrahs <79310230+dimasnugrahs@users.noreply.github.com>
2025-03-26 12:25:00 +0800

    first commit

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -73,7 +73,7 @@ const HeroSection = () => {
           <button
             key={i}
             onClick={() => setIndex(i)}
-            className={`w-4 h-4 rounded-full ${
+            className={`w-2 h-2 rounded-full ${
               i === index ? "bg-white" : "bg-gray-500"
             }`}
           ></button>
