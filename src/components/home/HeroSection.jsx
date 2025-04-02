6de3cb909477fd3fea0a47db83a1fcb5fc6173f9
dimasnugrahs <79310230+dimasnugrahs@users.noreply.github.com>
2025-04-02 17:42:56 +0800

    update herosection

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -5,15 +5,15 @@ const HeroSection = () => {
   return (
     <div className="flex flex-col lg:flex-row items-center justify-between bg-gray-100 w-full min-h-screen lg:px-32 px-6 ">
       <div className="lg:w-1/2 text-center lg:text-left space-y-6 lg:pt-0 pt-24">
-        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-900">
+        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-inter font-black text-company-950">
           Solusi Keuangan Anda Lebih Mudah, Cepat dan Terpercaya
         </h1>
-        <p className="text-gray-700 sm:text-base lg:text-lg">
+        <p className="text-company-950 text-lg">
           Kelola keuangan Anda dengan lebih mudah dan aman. Kami hadir untuk
           memberikan solusi keuangan yang cepat, transparan, dan terpercaya,
           membantu Anda mencapai tujuan finansial dengan lebih baik.
         </p>
-        <div className="flex flex-col sm:flex-row justify-center lg:justify-start space-y-3 sm:space-y-0 sm:space-x-4">
+        <div className="flex flex-col sm:flex-row justify-center lg:justify-start space-y-3 sm:space-y-0 sm:space-x-4 text-lg">
           <Link to="/services">
             <button className="px-6 py-2 border-2 border-company-500 text-company-500 rounded-lg hover:bg-company-500 hover:text-company-50 hover:border-company-500">
               Jelajahi Layanan
